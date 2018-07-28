b8c916c4459b6d96bb6f9f9648ffa8f74b2fa851
Sanjai S <sanjai.syamaprasad@gmail.com>
2018-07-28 08:17:55 -0400

    5 passing in todos

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 
 const express = require('express');
 const app = express();
-module.exports = app; // this line is only used to make testing easier.
+const morgan = require('morgan');
 
+module.exports = app; // this line is only used to make testing easier.
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + 'public'));
 // remember to plug in your router and any other middleware you may need here.
 
+app.get('/', (req, res, next) => {
+  res.send([]);
+});
 if (!module.parent) app.listen(3000); // conditional prevents a very esoteric EADDRINUSE issue with mocha watch + supertest + npm test.
--- models/todos.js
+++ models/todos.js
@@ -3,15 +3,21 @@
 let tasks = {}; // a place to store tasks by person
 
 module.exports = {
-  reset: function () {
+  reset: function() {
     tasks = {}; // (this function is completed for you.)
   },
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
-  listPeople: function () {
+  listPeople: function() {
     // returns an array of all people for whom tasks exist
+    const names = Object.keys(tasks);
+    return names;
   },
-  add: function (name, task) {
+  add: function(name, task) {
     // saves a task for a given person
-  }
+    !(name in tasks) ? (tasks[name] = [task]) : tasks[name].push(task);
+  },
+  list: function(name) {
+    return tasks[name];
+  },
   // etc.
 };
